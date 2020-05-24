6a7e2270f093e620e6364a2bdc37d30d4531d2c2
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 09:46:11 +1000

    Make form inputs enable to change values in EditItem component

--- client/src/components/EditItem.js
+++ client/src/components/EditItem.js
@@ -7,6 +7,15 @@ class EditItem extends Component{
             fullName: props.fullName || '',
             phone: props.phone || ''
         };
+        this.onChangeItem = this.onChangeItem.bind(this);
+    }
+
+    onChangeItem(event){
+        const target = event.target;
+        const name = target.name;
+        this.setState({
+            [name]: target.value
+        });
     }
 
     render(){
@@ -15,11 +24,11 @@ class EditItem extends Component{
             <form>
                 <div className="form-group">
                     <label hrmlFor="fullName">Full name</label>
-                    <input name="fullName" type="text" className="form-control" id="fullName" value={fullName}/>
+                    <input name="fullName" type="text" className="form-control" id="fullName" value={fullName} onChange={this.onChangeItem}/>
                 </div>
                 <div className="form-group">
                     <label hrmlFor="phone">Phone</label>
-                    <input name="phone" type="text" className="form-control" id="phone" value={phone} />
+                    <input name="phone" type="text" className="form-control" id="phone" value={phone} onChange={this.onChangeItem}/>
                 </div>                      
                 <div className="d-flex justify-content-start align-items-center mt-5">
                     <button type="button" className="btn btn-outline-danger mr-4" onClick={this.props.onCancel}>Cancel</button>
