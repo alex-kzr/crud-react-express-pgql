391a38a2b7b0557fd77aa348404c9d93a7ef4160
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 07:33:21 +1000

    Add ItemsBody component

---
+++ client/src/components/ItemsBody.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ItemsBody extends Component {
+    render(){
+        return(
+            <div>ListItems
+                <div>--ListItem</div>
+                ListItems
+            </div>
+        );
+    }    
+}
+
+export default ItemsBody;
