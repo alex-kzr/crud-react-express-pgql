f487c496d3bcd09e2ac01bab8d1b5041204d4133
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 07:54:57 +1000

    Add state to ItemsBody component. Add get api/users call.

--- client/src/components/ItemsBody.js
+++ client/src/components/ItemsBody.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import ItemsList from './ItemsList';
 import ItemListElement from './ItemListElement';
+import axios from 'axios';
 
 class ItemsBody extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: true,
+            users: {}
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/api/users').then(res => {
+            console.log(res);
+            this.setState({
+                loading: false,
+                users: res.data
+            });
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+    
+    
     render(){
+        if(this.state.loading === true){
+            return(
+                <h2>loading ...</h2>
+            );
+        }
         return(
             <ItemsList>
                 <ItemListElement text={'Item list atlast!'} />
