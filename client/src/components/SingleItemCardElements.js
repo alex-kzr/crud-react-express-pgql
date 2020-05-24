928b35a60fb4f4d238d2551353b0450dd67a7b69
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 09:14:07 +1000

    Add buttons Edit & Delete on SingleItemCardElemtents component

--- client/src/components/SingleItemCardElements.js
+++ client/src/components/SingleItemCardElements.js
@@ -10,6 +10,10 @@ const SingleItemCardElements = ({fullName, phone}) => {
                 <div className="card-text">
                     {phone}
                 </div>
+                <div className="d-flex justify-content-start ilign-items-end mt-5">
+                    <button className="btn btn-outline-danger mr-4">Delete</button>
+                    <button className="btn btn-outline-dark" >Edit</button>
+                </div>
             </div>
         </div>
     );
