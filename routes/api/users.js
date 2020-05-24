19bd4bf496cc2b1ec6ac0dd023712c6612e512e5
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 06:47:40 +1000

    Add ascending order by id to api/users get method

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../database');
 
 router.get('/', function(req, res){
-    db.select().from('users').then(function(data){
+    db.select().from('users').orderBy('id').then(function(data){
         res.send(data);
     }).catch(function(err){
         console.log(err);
