9c82820649d7fd9b6ca468d03ba91625cb38718f
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 06:17:53 +1000

    Add post method to api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,18 @@ const db = require('../../database');
 
 router.get('/', function(req, res){
     db.select().from('users').then(function(data){
-        console.log(data);
+        res.send(data);
     }).catch(function(err){
         console.log(err);
     });
+});
+
+router.post('/', function(req, res){
+    db.insert(req.body).returning('*').into('users').then(function(data){
+        res.send(data);
+    }).catch(function(err){
+        console.log(err);
+    });;    
 });
 
 module.exports = router; 
